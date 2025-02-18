8ceac6de288191e6c60d6343e39664c8a279dff2
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-02-18 14:32:20 +0900

    fix: 오타 수정

--- src/component/Sitemap.tsx
+++ src/component/Sitemap.tsx
@@ -9,7 +9,7 @@ export const Sitemap = () => {
         <MainContainer>
             <SitemapMenuItem onClick={ () => navigate("/music") }> 음악 </SitemapMenuItem>
             <SitemapMenuItem onClick={ () => navigate("/member") }> 멤버 </SitemapMenuItem>
-            <SitemapMenuItem onClick={ () => navigate("/album") }> 엘범 </SitemapMenuItem>
+            <SitemapMenuItem onClick={ () => navigate("/album") }> 앨범 </SitemapMenuItem>
             { /* <SitemapMenuItem onClick={ () => navigate("/announce") }> 소식 </SitemapMenuItem> */ }
         </MainContainer>
     )
